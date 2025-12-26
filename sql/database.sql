bb4184d8ea1db691974fe3eebd6f9c269dc2a265
balakoyyamani <balakoyyamani@gmail.com>
2025-12-26 21:35:08 +0530

    -sql_folder_updated

---
+++ sql/database.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE virtual_sensor_system;
+
+USE virtual_sensor_system;
+
+CREATE TABLE sensor_location (
+    location_id INT AUTO_INCREMENT PRIMARY KEY,
+    latitude DECIMAL(8,5) NOT NULL,
+    longitude DECIMAL(8,5) NOT NULL,
+    location_name VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE raw_environment_data (
+    data_id INT AUTO_INCREMENT PRIMARY KEY,
+    location_id INT NOT NULL,
+
+    temperature DECIMAL(5,2),
+    humidity INT,
+    wind_speed DECIMAL(5,2),
+    rainfall DECIMAL(5,2),
+    air_pressure INT,
+
+    recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_location
+    FOREIGN KEY (location_id)
+    REFERENCES sensor_location(location_id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE virtual_sensor_output (
+    output_id INT AUTO_INCREMENT PRIMARY KEY,
+    data_id INT NOT NULL,
+
+    risk_type VARCHAR(50),
+    risk_level VARCHAR(20),
+    description TEXT,
+
+    generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_raw_data
+    FOREIGN KEY (data_id)
+    REFERENCES raw_environment_data(data_id)
+    ON DELETE CASCADE
+);
