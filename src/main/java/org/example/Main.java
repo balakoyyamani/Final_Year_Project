dfe4bdadd12db66664be59510bf703f148e664bf
balakoyyamani <balakoyyamani@gmail.com>
2025-12-27 22:15:53 +0530

    -new_branch_upto_apicalls_every_seconds

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-
+//red_dragon_02
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
